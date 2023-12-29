a04eb43430c79dd6b391a6c572c66a55cca878d2
NoteSeonjin <hawooks112@gmail.com>
2023-12-29 14:01:29 +0900

    # 게시글 쓰기 페이지 생성

---
+++ src/main/java/com/spring/mvc/seonjinTest.java
@@ -0,0 +1,12 @@
+package com.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class seonjinTest {
+    @RequestMapping("/board")
+    public String Test() {
+        return "board/writepost";
+    }
+}
