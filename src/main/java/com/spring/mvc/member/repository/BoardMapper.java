bc888373a8b3da69eefa1845705205adef120540
rang_i <dregonball12@naver.com>
2024-01-05 14:15:56 +0900

    # header 아주 조금과 room을 만들때 roomMember가 만들어지 도록 수정 함

---
+++ src/main/java/com/spring/mvc/member/repository/BoardMapper.java
@@ -0,0 +1,21 @@
+package com.spring.mvc.member.repository;
+
+
+import com.spring.mvc.member.entity.Board;
+import com.spring.mvc.member.entity.Room;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+
+    //방 안에 게시글 을 만들것이다.
+    boolean save(Board board);
+
+    //방안에 게시글 전체 조회
+    List<Board> findAll();
+
+    //방안에 게시글을 수정 할 수 있다.
+    boolean modify(Board boardId);
+}
