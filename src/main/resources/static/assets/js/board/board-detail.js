03f0bc037598901a1269593e0ce5d414a064eb29
NoteSeonjin <hawooks112@gmail.com>
2024-01-06 01:04:29 +0900

    # HTML Element에 $ 추가

--- src/main/resources/static/assets/js/board/board-detail.js
+++ src/main/resources/static/assets/js/board/board-detail.js
@@ -2,50 +2,48 @@
 const $userReply = document.querySelectorAll(...['.comment']);
 const $replyBtnWrap = document.querySelector(".reply-btn-wrap")
 // 댓글 수정 버튼을 누르면 p태그가 input으로 바뀌는 js코드
-function editComment(comment) {
-    const commentText = comment.innerText;
-    const parentElement = comment.parentElement;
+function editComment($comment) {
+    const commentText =$comment.innerText;
+    const $parentElement = comment.parentElement;
 
-    const inputElement = document.createElement('input');
-    inputElement.type = 'text';
-    inputElement.classList.add('form-control')
-    inputElement.value = commentText;
+    const $inputElement = document.createElement('input');
+    $inputElement.type = 'text';
+    $inputElement.classList.add('form-control')
+    $inputElement.value = commentText;
 
     // Create a button element
-    const buttonElement = document.createElement('button');
-    buttonElement.type = 'button';
-    buttonElement.textContent = '저장';
-    buttonElement.classList.add('reply-save');
-    buttonElement.classList.add('btn-comment-up');
-
-    console.log(inputElement)
+    const $buttonElement = document.createElement('button');
+    $buttonElement.type = 'button';
+    $buttonElement.textContent = '저장';
+    $buttonElement.classList.add('reply-save');
+    $buttonElement.classList.add('btn-comment-up');
     // Replace the paragraph element with the input element
-    parentElement.replaceChild(inputElement, comment);
-    parentElement.appendChild(buttonElement);
+    $parentElement.replaceChild($inputElement, $comment);
+    $parentElement.appendChild($buttonElement);
     $replyBtnWrap.classList.add('non');
 
-    inputElement.focus();
+    $inputElement.focus();
 
     // Change the event listener of the "수정" button to save the changes
-    const modifyButton = parentElement.querySelector('.reply-save');
+    const modifyButton = $parentElement.querySelector('.reply-save');
     modifyButton.onclick = function () {
-        saveChanges(inputElement, comment,buttonElement);
+        saveChanges($inputElement,$buttonElement);
         $replyBtnWrap.classList.remove('non')
     };
 }
 
 /** 수정된 댓글 내용 저장하기 */
-function saveChanges(inputElement, commentParagraph, saveButton) {
-    const updatedText = inputElement.value;
+function saveChanges($inputElement, $saveButton) {
+    const updatedText = $inputElement.value;
 
     // Create a new paragraph element with the updated text
-    const newParagraph = document.createElement('p');
-    newParagraph.classList.add('reply-content');
-    newParagraph.innerText = updatedText;
+    const $newParagraph = document.createElement('p');
+    $newParagraph.classList.add('reply-content');
+    $newParagraph.innerText = updatedText;
 
     // Replace the input element and remove the save button
-    inputElement.parentElement.replaceChild(newParagraph, inputElement);
-    saveButton.remove();
+    $inputElement.parentElement.replaceChild($newParagraph, $inputElement);
+    $saveButton.remove();
 }
 // 댓글 수정 버튼 클릭시
 function modifyReplyClickHandler(e) {
