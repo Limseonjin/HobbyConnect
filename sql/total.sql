3e3a79401c275be563fece1267256d5ce7e72d95
NoteSeonjin <hawooks112@gmail.com>
2024-01-09 10:23:50 +0900

    # sql문 추가, 이미지 추가

--- sql/total.sql
+++ sql/total.sql
@@ -1,79 +1,85 @@
+
 Create Database hobby;
 
 use hobby;
 
-CREATE TABLE tbl_board
+CREATE TABLE tbl_person
 (
-    board_id      INTEGER AUTO_INCREMENT,
-    person_id     VARCHAR(16),
-    board_title   VARCHAR(20) NOT NULL,
-    board_content VARCHAR(400),
-    room_id       INTEGER,
-    view_count INTEGER,
+    person_id VARCHAR(50),
+    email   VARCHAR(255) NOT NULL ,
+    password VARCHAR(255) NOT NULL ,
+    nickname VARCHAR(50) NOT NULL ,
+    introduction VARCHAR(255),
     reg_date TIME DEFAULT current_timestamp,
-    CONSTRAINT pk_board PRIMARY KEY (board_id),
-    CONSTRAINT fk_board_person
-        FOREIGN KEY (person_id)
-            REFERENCES tbl_person (person_id),
-    CONSTRAINT fk_board_room
-        FOREIGN KEY (room_id)
-            REFERENCES tbl_room (room_id)
+    session_id VARCHAR(255),
+    limit_time DATETIME DEFAULT current_timestamp,
+    CONSTRAINT pk_person PRIMARY KEY (person_id)
 );
 
 CREATE TABLE tbl_room
 (
     room_id INTEGER AUTO_INCREMENT,
-    person_id VARCHAR(16),
-    room_name VARCHAR(20) NOT NULL,
-    room_pw VARCHAR(8),
+    person_id VARCHAR(50),
+    main_board_title VARCHAR(100) NOT NULL,
+    main_board_content VARCHAR(255),
+    room_name VARCHAR(100) NOT NULL,
+    room_pw VARCHAR(100),
     max_user INTEGER(255),
-    curr_user INTEGER(255),
-    reg_date TIME DEFAULT current_timestamp,
+    curr_user INTEGER(255) DEFAULT 1,
+    reg_date DATETIME DEFAULT current_timestamp,
     CONSTRAINT pk_room PRIMARY KEY (room_id),
     CONSTRAINT fk_room_person
         FOREIGN KEY (person_id)
-            REFERENCES tbl_person (person_id)
+            REFERENCES tbl_person (person_id) ON DELETE CASCADE
 );
 
-CREATE TABLE tbl_person
+CREATE TABLE tbl_board
 (
-    person_id VARCHAR(16),
-    email   VARCHAR(255) NOT NULL UNIQUE ,
-    password VARCHAR(20) NOT NULL UNIQUE ,
-    nickname VARCHAR(10) NOT NULL UNIQUE ,
-    location VARCHAR(255),
-    introduction VARCHAR(40),
-    reg_date TIME DEFAULT current_timestamp,
-    CONSTRAINT pk_person PRIMARY KEY (person_id)
+    board_id INTEGER AUTO_INCREMENT,
+    person_id     VARCHAR(50),
+    board_title   VARCHAR(255) NOT NULL,
+    board_content VARCHAR(800),
+    room_id       INTEGER,
+    view_count INTEGER DEFAULT 1,
+    reg_date DATETIME DEFAULT current_timestamp,
+    CONSTRAINT pk_board PRIMARY KEY (board_id),
+    CONSTRAINT fk_board_person
+        FOREIGN KEY (person_id)
+            REFERENCES tbl_person (person_id) ON DELETE CASCADE,
+    CONSTRAINT fk_board_room
+        FOREIGN KEY (room_id)
+            REFERENCES tbl_room (room_id) ON DELETE CASCADE
+
 );
 
 CREATE TABLE tbl_reply
 (
     reply_id  INTEGER AUTO_INCREMENT,
-    person_id VARCHAR(255) NOT NULL,
+    person_id VARCHAR(50) NOT NULL,
     board_id   INTEGER NOT NULL,
-    content   VARCHAR(200),
-    reg_date  TIME DEFAULT current_timestamp,
+    content   VARCHAR(255),
+    reg_date  DATETIME DEFAULT current_timestamp,
     CONSTRAINT pk_reply PRIMARY KEY (reply_id),
     CONSTRAINT fk_reply_person
         FOREIGN KEY (person_id)
-            REFERENCES tbl_person (person_id),
+            REFERENCES tbl_person (person_id) ON DELETE CASCADE,
     CONSTRAINT fk_reply_board
         FOREIGN KEY (board_id)
-            REFERENCES tbl_board (board_id)
+            REFERENCES tbl_board (board_id) ON DELETE CASCADE
 );
 
 CREATE TABLE tbl_roomMember
 (
     room_members INTEGER AUTO_INCREMENT,
     room_id INTEGER,
-    person_id VARCHAR(16),
-    authority VARCHAR(10) DEFAULT 'COMMON',
+    person_id VARCHAR(50),
+    auth VARCHAR(100) DEFAULT 'COMMON',
+    reg_date  DATETIME DEFAULT current_timestamp,
     CONSTRAINT pk_roomMember PRIMARY KEY (room_members),
     CONSTRAINT fk_roomMember_person
         FOREIGN KEY (person_id)
-            REFERENCES tbl_person (person_id),
+            REFERENCES tbl_person (person_id) ON DELETE CASCADE ,
     CONSTRAINT fk_roomMember_room
         FOREIGN KEY (room_id)
-            REFERENCES tbl_room (room_id)
+            REFERENCES tbl_room (room_id) ON DELETE CASCADE
 );
